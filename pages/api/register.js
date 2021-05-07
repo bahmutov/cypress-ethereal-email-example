0d3392d416fd1d8b35d0118a91b1526c9e43381b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-05-07 11:44:32 -0400

    use created confirmation code

--- pages/api/register.js
+++ pages/api/register.js
@@ -3,12 +3,18 @@ const { readFileSync } = require('fs')
 const { join } = require('path')
 const initEmailer = require('../../emailer')
 
+// singleton
+const codes = require('../../confirmation-codes')
+
 const confirmationEmailPath = join(
   process.cwd(), // should be the root folder of the project
   'emails',
   'confirmation-code.html',
 )
-const confirmationEmailHTML = readFileSync(confirmationEmailPath, 'utf-8')
+const confirmationEmailTemplateHTML = readFileSync(
+  confirmationEmailPath,
+  'utf-8',
+)
 
 export default async (req, res) => {
   if (req.method === 'POST') {
@@ -16,13 +22,20 @@ export default async (req, res) => {
     // return to the caller right away
     res.status(200).json({ name, email })
 
+    const confirmationCode = codes.createCode(email)
+    console.log('for %s confirmation code %s', email, confirmationCode)
+    const confirmationEmailHTML = confirmationEmailTemplateHTML.replace(
+      '%CONFIRMATION_CODE%',
+      confirmationCode,
+    )
+
     // and then send an email
     const emailer = await initEmailer()
     const info = await emailer.sendMail({
       from: '"Registration system" <reg@company.co>',
       to: email,
       subject: 'Confirmation code 1️⃣2️⃣3️⃣',
-      text: 'Your confirmation code is 654agc',
+      text: `Your confirmation code is ${confirmationCode}`,
       html: confirmationEmailHTML,
     })
     console.log('sent a confirmation email to %s', email)
