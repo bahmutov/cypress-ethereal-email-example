4f92babb2d84958d7deefb367ced5933beb9c464
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-05-10 08:56:17 -0400

    grab the email

--- receive.js
+++ receive.js
@@ -1,5 +1,8 @@
 var imaps = require('imap-simple')
 
+// run by injecting inbox login information
+// http://github.com/bahmutov/as-a
+// as-a cypress-ethereal-email-example-inbox node ./receive
 var config = {
   imap: {
     user: process.env.ETHEREAL_EMAIL,
@@ -11,31 +14,34 @@ var config = {
   },
 }
 
-imaps
-  .connect(config)
-  .then(function (connection) {
-    return connection.openBox('INBOX').then(function () {
-      var searchCriteria = ['1:5']
-      var fetchOptions = {
-        bodies: ['HEADER', 'TEXT'],
-      }
-      return connection
-        .search(searchCriteria, fetchOptions)
-        .then(function (messages) {
-          messages.forEach(function (item) {
-            console.log(item)
-            // var all = _.find(item.parts, { which: 'TEXT' })
-            // var html = Buffer.from(all.body, 'base64').toString('ascii')
-            // console.log(html)
-          })
-        })
-    })
+const seeEmails = async () => {
+  const connection = await imaps.connect(config)
+
+  await connection.openBox('INBOX')
+  var searchCriteria = ['1:5']
+  var fetchOptions = {
+    bodies: ['HEADER', 'TEXT'],
+  }
+  const messages = await connection.search(searchCriteria, fetchOptions)
+  messages.forEach(function (item) {
+    const [header, body] = item.parts
+    const message = {
+      to: header.body.to[0], // the first recipient
+      subject: header.body.subject[0],
+      // todo: extract plan and html email bodies
+      body: body.body,
+    }
+    console.log(message)
   })
-  .then(
-    () => {
-      console.log('all done')
-    },
-    (err) => {
-      console.error(err)
-    },
-  )
+
+  connection.end()
+}
+
+seeEmails().then(
+  () => {
+    console.log('all done')
+  },
+  (err) => {
+    console.error(err)
+  },
+)
