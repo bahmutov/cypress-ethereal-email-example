553a75674f68f01d2557f582591392b1a990976c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-05-11 08:46:19 -0400

    add comments

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -29,10 +29,14 @@ describe('Email confirmation', () => {
       .and('have.text', userEmail)
 
     // retry fetching the email
-    recurse(() => cy.task('getLastEmail'), Cypress._.isObject, {
-      timeout: 60000,
-      delay: 5000,
-    })
+    recurse(
+      () => cy.task('getLastEmail'), // Cypress commands to retry
+      Cypress._.isObject, // keep retrying until the task returns an object
+      {
+        timeout: 60000, // retry up to 1 minute
+        delay: 5000, // wait 5 seconds between attempts
+      },
+    )
       .its('html')
       .then((html) => {
         cy.document({ log: false }).invoke({ log: false }, 'write', html)
